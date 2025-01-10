5683a92e4e572332a5a311702f3de80caa35ec58
Aum <aumghodasara15@gmail.com>
2025-01-11 01:10:00 +0530

    area of work

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,13 @@
-import Image from 'next/image'
+import { motion } from 'framer-motion';
+import Image from "next/image";
+import { useInView } from "react-intersection-observer";
+
 
 export default function HeroAndAbout() {
+  const [ref, inView] = useInView({
+    triggerOnce: false, // Animation triggers every time the section comes into view
+    threshold: 0.2, // Trigger animation when 20% of the section is visible
+  });
   return (
     <div className="w-full bg-[#DEEDEF]">
       <section className="relative">
@@ -8,28 +15,27 @@ export default function HeroAndAbout() {
           <Image
             src="/hero2.jpg"
             alt="Hero Image"
-            width={600} 
-            height={900} 
+            width={600}
+            height={900}
             objectFit="cover"
             layout="responsive"
-            className='lg:block hidden'
+            className="lg:block hidden"
           />
-           <Image
-            src="/hero2.jpg" 
+          <Image
+            src="/hero2.jpg"
             alt="Hero Image"
-            width={600} 
-            height={900} 
+            width={600}
+            height={900}
             objectFit="cover"
             layout="responsive"
             style={{
-              aspectRatio: '1' 
+              aspectRatio: "1",
             }}
-            className='lg:hidden block'
+            className="lg:hidden block"
           />
-           
         </div>
 
-        <div className="bg-[#C0DBE1] lg:py-14 py-7 px-4 sm:px-6 lg:px-8 text-[#2E414B]">
+        <div className="bg-[#C0DBE1] lg:py-14 py-7 px-4 sm:px-6 lg:px-4 text-[#2E414B]">
           <div className="max-w-7xl mx-auto flex flex-col md:flex-row justify-between items-center">
             <div className="flex mx-auto items-center">
               <div className="mr-4">
@@ -44,7 +50,9 @@ export default function HeroAndAbout() {
                 <h2 className="text-center lg:text-3xl text-base font-bold mb-2">
                   JK POWER SYSTEM PVT LTD
                   <br />
-                  <span className="lg:text-xl text-sm font-normal">Government Approved Electrical Contractor</span>
+                  <span className="lg:text-xl text-sm font-normal">
+                    Government Approved Electrical Contractor
+                  </span>
                 </h2>
               </div>
             </div>
@@ -56,7 +64,9 @@ export default function HeroAndAbout() {
         <div className="max-w-7xl mx-auto">
           <div className="flex flex-col md:flex-row items-center gap-8">
             <div className="md:w-1/2">
-              <h2 className="text-center text-xl font-bold mb-7 py-2 w-2/3 bg-[#C0DBE1] rounded-xl mx-auto block lg:hidden">What We Do ?</h2>
+              <h2 className="text-center text-xl font-bold mb-7 py-2 w-2/3 bg-[#C0DBE1] rounded-xl mx-auto block lg:hidden">
+                What We Do ?
+              </h2>
               <Image
                 src="/hero2.jpg"
                 alt="About Company"
@@ -66,12 +76,24 @@ export default function HeroAndAbout() {
               />
             </div>
             <div className="md:w-1/2">
-              <h2 className="text-center lg:text-3xl text-base font-bold lg:mb-12 mb-7 py-2 w-2/3 bg-[#C0DBE1] lg:rounded-l-2xl mx-auto lg:block hidden">What We Do ?</h2>
+              <h2 className="text-center lg:text-3xl text-base font-bold lg:mb-12 mb-7 py-2 w-2/3 bg-[#C0DBE1] lg:rounded-l-2xl mx-auto lg:block hidden">
+                What We Do ?
+              </h2>
               <p className="text-lg ">
-                JK Power Industry is a leading provider of industrial and commercial electrical solutions, specializing in the installation and maintenance of electrical systems. As a government-approved contractor, we ensure efficient and safe operations across various industries, backed by our expertise and commitment to quality.
+                JK Power Industry is a leading provider of industrial and
+                commercial electrical solutions, specializing in the
+                installation and maintenance of electrical systems. As a
+                government-approved contractor, we ensure efficient and safe
+                operations across various industries, backed by our expertise
+                and commitment to quality.
               </p>
               <p className="text-lg mt-4">
-                In addition to our contracting services, we offer a wide range of high-quality electrical products and custom panel manufacturing. Our focus on innovation and reliability enables us to deliver solutions that drive operational efficiency and sustainability, making JK Power Industry a trusted partner for powering progress.
+                In addition to our contracting services, we offer a wide range
+                of high-quality electrical products and custom panel
+                manufacturing. Our focus on innovation and reliability enables
+                us to deliver solutions that drive operational efficiency and
+                sustainability, making JK Power Industry a trusted partner for
+                powering progress.
               </p>
             </div>
           </div>
@@ -80,9 +102,10 @@ export default function HeroAndAbout() {
 
       <section className="lg:py-12 px-7 py-6 lg:px-8">
         <div className="max-w-7xl mx-auto text-center">
-          <h2 className="lg:text-3xl text-xl font-bold text-[#2E414B] lg:mb-12 mb-7 bg-[#C0DBE1] py-2 rounded-2xl lg:w-3/12 w-2/3 mx-auto">Core Services</h2>
+          <h2 className="lg:text-3xl text-xl font-bold text-[#2E414B] lg:mb-12 mb-7 bg-[#C0DBE1] py-2 rounded-2xl lg:w-3/12 w-2/3 mx-auto">
+            Core Services
+          </h2>
           <div className="grid grid-cols-1 md:grid-cols-3 lg:gap-8 gap-4">
-
             {/* First Service */}
             <div className="overflow-hidden rounded-xl transition-transform transform border-4 border-[#BD7500]">
               <div className="lg:px-5 lg:py-4 p-2">
@@ -104,8 +127,7 @@ export default function HeroAndAbout() {
                 </div>
                 <h3 className="lg:text-xl text-base font-semibold text-gray-900 mb-2 text-center">
                   - Electrical Contracting Service <br />
-                  - Project Activity <br />
-                  - Maintenance Activity
+                  - Project Activity <br />- Maintenance Activity
                 </h3>
               </div>
             </div>
@@ -130,7 +152,8 @@ export default function HeroAndAbout() {
                   />
                 </div>
                 <h3 className="lg:text-xl text-base font-semibold text-gray-900 mb-2 text-center">
-                  - All Types of Industrial <br />and Commercial Material Supply
+                  - All Types of Industrial <br />
+                  and Commercial Material Supply
                 </h3>
               </div>
             </div>
@@ -160,10 +183,98 @@ export default function HeroAndAbout() {
               </div>
             </div>
           </div>
+        </div>
+      </section>
 
+      <section className="lg:py-28 px-7 py-6 lg:px-8">
+        <div className="max-w-7xl mx-auto text-center">
+          <div className="flex flex-wrap justify-center mb-7" ref={ref}>
+            {Array.from({ length: 4 }).map((_, index) => (
+              <motion.div
+                key={index}
+                initial={{ opacity: 0, y: 50 }}
+                animate={inView ? { opacity: 1, y: 0 } : { opacity: 0, y: 50 }}
+                transition={{ duration: 0.5, delay: 0 * index }}
+                className="mx-2"
+              >
+                <Image
+                  src="/power.jpg"
+                  alt="Service 3"
+                  width={300}
+                  height={200}
+                  className="rounded-lg"
+                />
+              </motion.div>
+            ))}
+          </div>
+
+          <motion.div
+            ref={ref}
+            className="flex flex-wrap justify-center items-center mb-6"
+          >
+            {/* Left Image - Animated from Left */}
+            <motion.div
+              initial={{ opacity: 0, x: 200 }}
+              animate={inView ? { opacity: 1, x: 0 } : { opacity: 0, x: 300 }}
+              transition={{ duration: 0.5, delay: 0 }}
+              className="mx-4"
+            >
+              <Image
+                src="/power.jpg"
+                alt="Service 3"
+                width={300}
+                height={200}
+                className="rounded-lg"
+              />
+            </motion.div>
+
+            {/* Heading */}
+            <motion.h2
+              ref={ref}
+              initial={{ opacity: 0, scale: 0.8 }}
+              animate={inView ? { opacity: 1, scale: 1 } : { opacity: 1, scale: 0.8 }}
+              className="lg:text-3xl text-xl font-bold text-[#2E414B] lg:mb-12 mb-7 bg-[#C0DBE1] py-7 rounded-2xl lg:w-2/6 w-2/3 m-auto"
+            >
+              OUR AREA OF WORK
+            </motion.h2>
+            {/* Right Image - Animated from Right */}
+            <motion.div
+              initial={{ opacity: 0, x: -200 }}
+              animate={inView ? { opacity: 1, x: 0 } : { opacity: 0, x: -300 }}
+              transition={{ duration: 0.5, delay: 0 }}
+              className="mx-4"
+            >
+              <Image
+                src="/power.jpg"
+                alt="Service 3"
+                width={300}
+                height={200}
+                className="rounded-lg"
+              />
+            </motion.div>
+          </motion.div>
+
+          <div className="flex flex-wrap justify-center lg:mb-12 mb-7" ref={ref}>
+            {Array.from({ length: 4 }).map((_, index) => (
+              <motion.div
+                key={index}
+                initial={{ opacity: 0, y: -50 }}
+                animate={inView ? { opacity: 1, y: 0 } : { opacity: 0, y: -50 }}
+                transition={{ duration: 0.5, delay: 0 * index }}
+                className="mx-2"
+              >
+                <Image
+                  src="/power.jpg"
+                  alt="Service 3"
+                  width={300}
+                  height={200}
+                  className="rounded-lg"
+                />
+              </motion.div>
+            ))}
+          </div>
         </div>
       </section>
     </div>
-  )
+  );
 }
-
