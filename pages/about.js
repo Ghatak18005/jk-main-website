fe78ff4921e7c0db410fd6c65a7991f0129c3700
Aum <aumghodasara15@gmail.com>
2025-01-11 19:12:39 +0530

    animations done

--- pages/about.js
+++ pages/about.js
@@ -1,86 +1,126 @@
+import Head from "next/head";
 import Image from "next/image";
+import { motion } from "framer-motion";
 
-export default function about() {
-  return (
-    <div className="min-h-screen bg-[#DEEDEF] overflow-hidden">
-      <section className="relative h-[60vh] w-full">
+const FounderProfile = ({ name, image, description, reverse, direction }) => (
+  <motion.div
+    initial={{ opacity: 0, x: direction === "right" ? 100 : -100 }}
+    whileInView={{ opacity: 1, x: 0 }}
+    transition={{ duration: 1.3, ease: "easeOut" }}
+    viewport={{ once: true }}
+    className={`flex flex-col ${reverse ? "md:flex-row-reverse" : "md:flex-row"} items-center mb-16`}
+  >
+    <div className={`md:w-1/2 mb-6 ${reverse ? "md:pl-8" : "md:pr-8"} flex justify-center`}>
+      <motion.div
+        transition={{ duration: 0.3 }}
+      >
         <Image
-          src="/about_us/hero_section.png"
-          alt="Company Hero Image"
-          fill
-          className="object-cover"
-          priority
+          src={image}
+          alt={`${name}'s Picture`}
+          width={300}
+          height={300}
+          className="rounded-2xl"
         />
-        <div className="absolute inset-0 bg-black bg-opacity-50 flex items-center justify-center">
-          <h1 className="text-4xl md:text-6xl font-bold text-white text-center">
-            Welcome to Our Company
-          </h1>
-        </div>
-      </section>
-      <section className="py-16 px-4 max-w-4xl mx-auto">
-        <h2 className=" text-3xl md:text-5xl font-bold mb-6 text-center text-[#2E414B]">
-          About Our Company
-        </h2>
-        <p className="text-xl text-[#2E414B] leading-relaxed">
-          Our company is dedicated to innovation and excellence in every aspect
-          of our work. We strive to create solutions that make a positive impact
-          on the world, focusing on sustainability, technology, and
-          human-centered design. With a team of passionate experts and a
-          commitment to continuous improvement, we're shaping the future of our
-          industry.
-        </p>
-      </section>
-      <section className="py-16 px-4 max-w-5xl mx-auto">
-        <h2 className=" text-3xl md:text-5xl font-bold mb-12 text-center text-[#2E414B]">
-          Our Founders
-        </h2>
-        <div className="flex flex-col md:flex-row items-center mb-16">
-          <div className="md:w-1/2 mb-6 md:mb-0 md:pr-8">
-            <Image
-              src="/about_us/person_placeholder.png"
-              alt="Founder 1"
-              width={300}
-              height={300}
-              className="rounded-full hover:scale-105 transition-transform duration-200 "
-            />
-          </div>
-          <div className="md:w-1/2">
-            <h3 className="text-2xl font-semibold mb-4 text-[#2E414B]">
-              Prit Desai
-            </h3>
-            <p className="text-lg text-gray-700 leading-relaxed">
-              Jane Doe is a visionary leader with over 15 years of experience in
-              the tech industry. Her innovative approach and strategic thinking
-              have been instrumental in our company's growth and success. Jane
-              is passionate about mentoring young entrepreneurs and promoting
-              diversity in the workplace.
-            </p>
-          </div>
-        </div>
-        <div className="flex flex-col md:flex-row-reverse items-center">
-          <div className="md:w-1/2 mb-6 md:pl-8 flex justify-center">
-            <Image
-              src="/about_us/person_placeholder.png"
-              alt="Founder 2"
-              width={300}
-              height={300}
-              className="rounded-full hover:scale-105 transition-transform duration-200"
-            />
-          </div>
-          <div className="md:w-1/2">
-            <h3 className="text-2xl font-semibold mb-4 text-[#2E414B]">
-              Mihir Gohil
-            </h3>
-            <p className="text-lg text-gray-700 leading-relaxed">
-              John Smith brings a wealth of experience in product development
-              and market strategy. With a background in both engineering and
-              business, John has a unique perspective that drives our company's
-              innovative solutions. He is committed to creating products that
-              not only meet but exceed customer expectations.
-            </p>
-          </div>
-        </div>
-      </section>
+      </motion.div>
     </div>
+    {/*md:w-1/2*/}
+    <div className="">
+      <h3 className="text-2xl font-semibold mb-4 text-[#2E414B] text-center md:text-left">{name}</h3>
+      <p className="lg:text-xl text-lg text-gray-700 leading-relaxed ">{description}</p>
+    </div>
+  </motion.div>
+);
+
+export default function About() {
+  return (
+    <>
+      <Head>
+        <title>About Us | Our Company</title>
+        <meta
+          name="description"
+          content="Learn about our company's mission, values, and founders."
+        />
+      </Head>
+      <div className="min-h-screen bg-[#DEEDEF] overflow-hidden">
+        <motion.section
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1 }}
+          transition={{ duration: 1 }}
+          className="relative h-[60vh] w-full"
+        >
+          <Image
+            src="/about_us/hero_section.png"
+            alt="Company Hero Image"
+            fill
+            className="object-cover"
+            priority
+          />
+          <motion.div
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            transition={{ delay: 0.5, duration: 0.5 }}
+            className="absolute inset-0 bg-black bg-opacity-50 flex items-center justify-center"
+          >
+            <h1 className="text-4xl md:text-6xl font-bold text-white text-center">
+              Welcome to Our Company
+            </h1>
+          </motion.div>
+        </motion.section>
+
+        {/* About Us Section */}
+        <motion.section
+          initial={{ opacity: 0, y: 50 }}
+          whileInView={{ opacity: 1, y: 0 }}
+          transition={{ duration: 0.8 }}
+          viewport={{ once: true }}
+          className="lg:py-16 lg:px-4 max-w-7xl mx-auto py-6 px-4"
+        >
+          <h2 className="text-2xl md:text-3xl font-bold mb-6 text-center text-[#2E414B] bg-[#C0DBE1] lg:w-1/4 w-1/2 rounded-full p-2 mx-auto">
+            About Us
+          </h2>
+          <p className="text-lg lg:text-xl text-[#2E414B] leading-relaxed px-4 lg:px-7">
+            Our company is dedicated to innovation and excellence in every
+            aspect of our work. We strive to create solutions that make a
+            positive impact on the world, focusing on sustainability,
+            technology, and human-centered design. With a team of passionate
+            experts and a commitment to continuous improvement, we're shaping
+            the future of our industry.
+          </p>
+        </motion.section>
+
+        {/* Founders Section */}
+        <section className="lg:py-16 lg:px-4 py-6 px-7 sm:px-6 max-w-7xl mx-auto">
+          <motion.h2
+            initial={{ opacity: 0, y: -20 }}
+            animate={{ opacity: 1, y: 0 }}
+            transition={{ duration: 0.8 }}
+            className="text-2xl md:text-3xl font-bold mb-12 text-center text-[#2E414B] bg-[#C0DBE1] lg:w-1/3 w-full rounded-full p-2 mx-auto"
+          >
+            About Originators
+          </motion.h2>
+          <FounderProfile
+            name="Prit Desai"
+            image="/about/person.png"
+            description="Prit Desai is a visionary leader with over 15 years of experience in
+            the tech industry. His innovative approach and strategic thinking
+            have been instrumental in our company's growth and success. Prit
+            is passionate about mentoring young entrepreneurs and promoting
+            diversity in the workplace."
+            direction="right"
+          />
+          <FounderProfile
+            name="Mihir Gohil"
+            image="/about/person.png"
+            description="Mihir Gohil brings a wealth of experience in product development
+            and market strategy. With a background in both engineering and
+            business, Mihir has a unique perspective that drives our company's
+            innovative solutions. He is committed to creating products that
+            not only meet but exceed customer expectations."
+            direction="left"
+            reverse
+          />
+        </section>
+      </div>
+    </>
   );
 }
--- pages/products.js
+++ pages/products.js
@@ -1,5 +1,7 @@
 import Image from "next/image";
 import { useEffect, useState } from "react";
+import { motion, AnimatePresence } from "framer-motion";
+
 export default function Products() {
   const categoryData = [
     {
@@ -7,102 +9,32 @@ export default function Products() {
       description: "Switchgear ensures safe power control and protection, while automation enhances efficiency and monitoring in electrical systems.",
       imgSrc: "/products/head_img/head1.jpg",
       products: [
-        {
-          name: "Phillips Screwdriver",
-          imgSrc: "/products/pro_img/img1.png",
-        },
-        {
-          name: "Flathead Screwdriver",
-          imgSrc: "/products/pro_img/img1.png",
-        },
-        {
-          name: "Precision Screwdriver",
-          imgSrc: "/products/pro_img/img1.png",
-        }
-      ]
+        { name: "Phillips Screwdriver", imgSrc: "/products/pro_img/img1.png" },
+        { name: "Flathead Screwdriver", imgSrc: "/products/pro_img/img1.png" },
+        { name: "Precision Screwdriver", imgSrc: "/products/pro_img/img1.png" },
+      ],
     },
     {
       title: "Wires and Cables",
       description: "Wires and cables are essential for transmitting electrical power and signals, offering reliable and durable connections for diverse applications in industries, homes, and infrastructure.",
       imgSrc: "/products/head_img/head2.jpg",
       products: [
-        {
-          name: "Cordless Drill Pro",
-          imgSrc: "/products/pro_img/img1.png",
-        },
-        {
-          name: "Impact Drill Max",
-          imgSrc: "/products/pro_img/img1.png",
-        },
-        {
-          name: "Compact Drill Lite",
-          imgSrc: "/products/pro_img/img1.png",
-        }
-      ]
+        { name: "Cordless Drill Pro", imgSrc: "/products/pro_img/img1.png" },
+        { name: "Impact Drill Max", imgSrc: "/products/pro_img/img1.png" },
+        { name: "Compact Drill Lite", imgSrc: "/products/pro_img/img1.png" },
+      ],
     },
     {
       title: "Industrial Light",
       description: "Industrial lights provide bright, energy-efficient illumination for large-scale environments, enhancing safety and productivity.",
       imgSrc: "/products/head_img/head3.jpg",
       products: [
-        {
-          name: "Cordless Drill Pro",
-          imgSrc: "/products/pro_img/img1.png",
-        },
-        {
-          name: "Impact Drill Max",
-          imgSrc: "/products/pro_img/img1.png",
-        },
-        {
-          name: "Compact Drill Lite",
-          imgSrc: "/products/pro_img/img1.png",
-        },
-        {
-          name: "Impact Drill Max",
-          imgSrc: "/products/pro_img/img1.png",
-        },
-        {
-          name: "Compact Drill Lite",
-          imgSrc: "/products/pro_img/img1.png",
-        },
-        {
-          name: "Impact Drill Max",
-          imgSrc: "/products/pro_img/img1.png",
-        },
-        {
-          name: "Compact Drill Lite",
-          imgSrc: "/products/pro_img/img1.png",
-        },
-        {
-          name: "Impact Drill Max",
-          imgSrc: "/products/pro_img/img1.png",
-        },
-        {
-          name: "Compact Drill Lite",
-          imgSrc: "/products/pro_img/img1.png",
-        }
-      ]
+        { name: "Cordless Drill Pro", imgSrc: "/products/pro_img/img1.png" },
+        { name: "Impact Drill Max", imgSrc: "/products/pro_img/img1.png" },
+        { name: "Compact Drill Lite", imgSrc: "/products/pro_img/img1.png" },
+      ],
     },
-    {
-      title: "Cable Tray",
-      description: "A cable tray is a support system used to organize and protect electrical cables, ensuring safe and efficient cable management in industrial and commercial settings.",
-      imgSrc: "/products/head_img/head4.jpg",
-      products: [
-        {
-          name: "Cordless Drill Pro",
-          imgSrc: "/products/pro_img/img1.png",
-        },
-        {
-          name: "Impact Drill Max",
-          imgSrc: "/products/pro_img/img1.png",
-        },
-        {
-          name: "Compact Drill Lite",
-          imgSrc: "/products/pro_img/img1.png",
-        }
-      ]
-    },
-
+    
   ];
 
   const [filteredCategories, setFilteredCategories] = useState(categoryData);
@@ -111,9 +43,10 @@ export default function Products() {
 
   const handleSearch = (event) => {
     const query = event.target.value.toLowerCase();
-    const filtered = categoryData.filter((category) =>
-      category.title.toLowerCase().includes(query) ||
-      category.description.toLowerCase().includes(query)
+    const filtered = categoryData.filter(
+      (category) =>
+        category.title.toLowerCase().includes(query) ||
+        category.description.toLowerCase().includes(query)
     );
     setFilteredCategories(filtered);
   };
@@ -121,117 +54,131 @@ export default function Products() {
   const showOverlay = (category) => {
     setSelectedCategory(category);
     setIsOverlayVisible(true);
-    document.body.style.overflow = 'hidden';
+    document.body.style.overflow = "hidden";
   };
 
   const hideOverlay = () => {
     setIsOverlayVisible(false);
     setSelectedCategory(null);
-    document.body.style.overflow = 'auto';
+    document.body.style.overflow = "auto";
   };
 
-  // Handle click outside of overlay cards
   const handleOverlayClick = (e) => {
-    if (e.target.classList.contains('overlay-background')) {
+    if (e.target.classList.contains("overlay-background")) {
       hideOverlay();
     }
   };
 
   return (
-    <div className="bg-[#DEEDEF] min-h-screen overflow-hidden">
-
-
-      <div className={`p-6 transition-all duration-300 ${isOverlayVisible ? 'blur-sm' : ''}`}>
-        <div className="flex justify-center mb-6 bg-[#C0DBE1] rounded-full lg:mx-64 md:mx-32 mx-auto">
+    <div className="bg-[#DEEDEF] min-h-screen overflow-hidden lg:px-5 px-3">
+      {/* Search Bar */}
+      <motion.div
+        initial={{ opacity: 0, y: -50 }}
+        animate={{ opacity: 1, y: 0 }}
+        transition={{ duration: 0.8 }}
+        className={`p-6 ${isOverlayVisible ? "blur-sm" : ""}`}
+      >
+        <div className="flex justify-center mb-6 bg-[#C0DBE1] rounded-full lg:w-1/2 w-full mx-auto">
           <div className="relative flex w-full">
-            <button className="px-2 py-2">
-              <Image src="/products/hamburger.svg" alt="Menu" width={56} height={56} />
-            </button>
             <input
               type="text"
               onChange={handleSearch}
               placeholder="Search products..."
-              className="w-full py-2 bg-[#C0DBE1] focus:outline-none text-black"
+              className="w-full py-2 px-8 bg-[#C0DBE1] focus:outline-none text-black rounded-full"
             />
             <button className="px-4 py-2">
-              <Image src="/products/search.svg" alt="Search" width={56} height={56} />
-            </button>
-
-          </div>
-        </div>
-
-        <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-3 lg:gap-6">
-          {filteredCategories.map((category, index) => (
-            <div
-              key={index}
-              className="bg-[#2D5F6B] text-white rounded-lg shadow-md flex flex-col p-4 transform transition-transform"
-            >
-              <div className="relative w-full h-64 rounded-lg overflow-hidden">
-                <Image
-                  src={category.imgSrc}
-                  alt={category.title}
-                  layout="fill"        
-                  objectFit="cover"    
-                  className="rounded-lg"
-                />
-              </div>
-
-              <div className="pt-4 flex-grow ">
-                <h3 className="text-[#DEEDEF] text-base md:text-lg font-semibold lg:mb-2 mb-1">
-                  {category.title}
-                </h3>
-                <p className="text-sm lg:text-base text-[#C0DBE1] lg:mb-4 mb-2">
-                  {category.description}
-                </p>
-              </div>
-              <button
-                className="bg-[#EFAA00] text-[#2E414B] px-4 py-2 mx-auto rounded-full lg:w-3/4 w-3/4 hover:bg-yellow-600 transition-colors lg:text-sm text-xs
-              "
-                onClick={() => showOverlay(category)}
-              >
-                Explore More
-              </button>
-            </div>
-          ))}
-        </div>
-      </div>
-
-      {isOverlayVisible && (
-      <div
-      className="fixed inset-0 bg-black bg-opacity-70 z-20 flex items-center justify-center overlay-background"
-      onClick={handleOverlayClick}
-    >
-      <button
-        className="absolute top-14 lg:top-12 right-6 lg:right-32 bg-[#EFAA00] text-[#2E414B] rounded-full px-2 py-1 lg:px-4 lg:py-2 hover:bg-yellow-600 transition-colors z-10"
-        onClick={hideOverlay}
-      >
-        ✕
-      </button>
-    
-      <div className="relative max-h-[75vh] lg:max-h-[80vh] overflow-y-auto grid grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-6 p-4 bg-[#2D5F6B] rounded-lg shadow-md w-5/6 lg:w-3/4 ">
-        {selectedCategory?.products.map((product, index) => (
-          <div
-            key={index}
-            className="w-full bg-[#598088] rounded-lg flex flex-col justify-between lg:p-6 p-3 shadow-md transform transition-transform"
-          >
-            <div className="text-center h-full rounded-lg lg:p-2 p-1">
               <Image
-                src={product.imgSrc}
-                alt={product.name}
-                width={200}
-                height={200}
-                className="mx-auto mb-2 lg:mb-4 rounded-lg"
+                src="/products/search.svg"
+                alt="Search"
+                width={56}
+                height={56}
               />
-            </div>
-            <div className="text-[#DEEDEF] text-center py-0 lg:py-2 lg:text-xl text-sm">{product.name}</div>
+            </button>
           </div>
-        ))}
-      </div>
-    </div>
-    
-     
-      )}
+        </div>
+      </motion.div>
 
+      {/* Categories Grid */}
+      <motion.div
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
+        transition={{ duration: 0.8, delay: 0.2 }}
+        className="grid grid-cols-1 gap-4 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-3 lg:gap-6"
+      >
+        {filteredCategories.map((category, index) => (
+          <motion.div
+            key={index}
+            className="bg-[#2D5F6B] text-white rounded-lg shadow-md flex flex-col p-4 w-full mx-auto"
+          >
+            <motion.div
+              className="relative w-full h-60 rounded-lg overflow-hidden"
+              initial={{ opacity: 0, scale: 0.9 }}
+              animate={{ opacity: 1, scale: 1 }}
+              transition={{ duration: 0.6, delay: index * 0.1 }}
+            >
+              <Image
+                src={category.imgSrc}
+                alt={category.title}
+                layout="fill"
+                objectFit="cover"
+                className="rounded-lg"
+              />
+            </motion.div>
+            <div className="pt-4 flex-grow">
+              <h3 className="text-[#DEEDEF] text-base md:text-lg font-semibold lg:mb-2 mb-1">
+                {category.title}
+              </h3>
+              <p className="text-sm lg:text-base text-[#C0DBE1] lg:mb-4 mb-2">
+                {category.description}
+              </p>
+            </div>
+            <button
+              className="bg-[#EFAA00] text-[#2E414B] px-4 py-2 mx-auto rounded-full lg:w-3/4 w-3/4 hover:bg-yellow-600 transition-colors lg:text-sm text-xs"
+              onClick={() => showOverlay(category)}
+            >
+              Explore More
+            </button>
+          </motion.div>
+        ))}
+      </motion.div>
+
+      {/* Overlay */}
+      <AnimatePresence>
+        {isOverlayVisible && (
+          <motion.div
+            className="fixed inset-0 bg-black bg-opacity-70 z-20 flex items-center justify-center overlay-background"
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            exit={{ opacity: 0 }}
+            onClick={handleOverlayClick}
+          >
+            <motion.div
+              className="relative overflow-y-auto grid grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-6 p-4 bg-[#2D5F6B] rounded-lg shadow-md w-5/6 lg:w-3/4"
+              initial={{ scale: 0.9 }}
+              animate={{ scale: 1 }}
+              exit={{ scale: 0.9 }}
+            >
+              {selectedCategory?.products.map((product, index) => (
+                <div
+                  key={index}
+                  className="w-full bg-[#598088] rounded-lg flex flex-col justify-between lg:p-6 p-3 shadow-md"
+                >
+                  <Image
+                    src={product.imgSrc}
+                    alt={product.name}
+                    width={200}
+                    height={200}
+                    className="mx-auto mb-2 lg:mb-4 rounded-lg"
+                  />
+                  <div className="text-[#DEEDEF] text-center py-0 lg:py-2 lg:text-xl text-sm">
+                    {product.name}
+                  </div>
+                </div>
+              ))}
+            </motion.div>
+          </motion.div>
+        )}
+      </AnimatePresence>
     </div>
   );
 }
